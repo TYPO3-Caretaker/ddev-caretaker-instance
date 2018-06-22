66e800d0da0d4264bc8e3f48f88daf4733479f14
Tobias Liebig <work@etobi.de>
2018-06-22 18:45:58 +0200

    [TASK] initial TYPO3 setup

---
+++ web/typo3conf/LocalConfiguration.php
@@ -0,0 +1,94 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'explicitADmode' => 'explicitAllow',
+        'installToolPassword' => '$P$C5Tvx.Qid12E2VQB2O0tZCU7sIijI00',
+        'loginSecurityLevel' => 'normal',
+    ],
+    'DB' => [
+        'database' => 'db',
+        'host' => 'db',
+        'password' => 'db',
+        'port' => 3306,
+        'username' => 'db',
+    ],
+    'EXT' => [
+        'extConf' => [
+            'caretaker_instance' => 'a:2:{s:7:"crypto.";a:2:{s:9:"instance.";a:2:{s:9:"publicKey";s:451:"-----BEGIN PUBLIC KEY-----|MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsqupZpSjd+Fas31AU7h6|3QJqUcgzXmQ7K6Y7n0as4QGiv3HC7yI6bKhK8zorELqnv6PiJM2cod2xZC8shYMz|qZI6kMDj2VlXq/LVAMQPKERtOBoZsdWYNNRFLBVfT420MCicgZF7n5xAhF8aAHWE|XITWjImnQElOMi5GlDctusT/LtjVpYLV2uJ24xWotUS+ImHBp4dv3nkpPD7eONOJ|0Wejy5Wptj6kZBZRgQQgHk3CxEkbfldEiplTXnZs/GRGsbz0kQqak36tTBi4ncUX|XKXmtiVtDMbYPNEF6MVD9BDtTwB90G57ROcbAWJij3EiQmIKifHYvOPWUxk9fZ69|zQIDAQAB|-----END PUBLIC KEY-----|";s:10:"privateKey";s:1704:"-----BEGIN PRIVATE KEY-----|MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCyq6lmlKN34Vqz|fUBTuHrdAmpRyDNeZDsrpjufRqzhAaK/ccLvIjpsqErzOisQuqe/o+IkzZyh3bFk|LyyFgzOpkjqQwOPZWVer8tUAxA8oRG04Ghmx1Zg01EUsFV9PjbQwKJyBkXufnECE|XxoAdYRchNaMiadASU4yLkaUNy26xP8u2NWlgtXa4nbjFai1RL4iYcGnh2/eeSk8|Pt4404nRZ6PLlam2PqRkFlGBBCAeTcLESRt+V0SKmVNedmz8ZEaxvPSRCpqTfq1M|GLidxRdcpea2JW0Mxtg80QXoxUP0EO1PAH3QbntE5xsBYmKPcSJCYgqJ8di849ZT|GT19nr3NAgMBAAECggEAWgNDp8twg3/ECzMpykPhmiCSYbtDreU/LiFkXjzj8cO6|M0rj00fiOUWb5OfENHxk8JZm4vW3WJDwwrFFc8oCqAZkfPLAl4p8McN37hfYLwgy|IhZ8JWyfbtYWNzxZNSIFylFbb6dUqbe1hTrIRbzuvxGpaUocObX0sDFCG3+lCTdI|aW1kbVTQBauD//LjWfvKMItWiSq8jMN9qsp1gKs3QxAEmDBHCupoyFnO2FG3H2df|TjEKJH9HKjhHsrbpVRRFMtoIHaD6nLug1tIxwHbm09rBKG/icoS0Fvbh6hIObFNu|nCUnQ6OvWWsClitmG15+xC+Qh0p0LBOa8jbPdyTTPQKBgQDbypTQfr2BMEAg9xFk|Fb39mwCWACJpH7BCwKWK+1pawzA7pB2jYxiLVBCgYe2Stil3Dr8g9h2JpRBLHdjf|oqpMKucw+e8YfozWu/mL+75DSRv0jiQ3Ihzxps8Tcq8VsrB8TmO9okucaNHgPdru|SLTl3bkEq1ex/ERV8YyAMhBVXwKBgQDQGt0zdT4y1XvnWDIM9hAg8ShYCgqVQJBb|nS2/68yg3kIj9bcROY75pZ2BZ6hDEoHkN7wEhbfEZBH9/JimykHdoM7HPq1X55Gg|lD0ZRl3UP7DkXnE6PXKPBwAyHFzH7auQ/TvL958FXvXprOl7CA71M8lvY59rk61h|Vk0bt/bwUwKBgB6UOswfNgJpUiLD/RltpPdJv3UfI6p4jC9dWirR+aozCdziYnU+|7kVENuP5e/IzgeVcmd7eHxHtcyzgRDIpEYRzW6MGwusngDTIBlohdkEqRMdrl0JM|brZ4PMs5434jVEaXKW1SzS4/ryOBt5n7Z5+rj3hE1nie3msOpjuXu4oFAoGAZ3KT|Nvs3Ggnpd8ztxGelCNsYVPtY22ym1gPCND9BD06szjdc82ELY6oiK6udhwnyK8JQ|4maePTAJiZpBP9gW+dbwMzdC8Ama6JoH7UJ/bfzjMAKKgor9IYA3ZrGWpYPN31si|mHfVQtQ7phdgUNpmpRlDLu3014MnktVVq0u9oyUCgYEAwoLQHQ3OCKEXpL2yFPRR|XxPYNaNSrsL0sdAzFs8gPBN1iAX2t5au21gLteZV0Em3wiPvQG8PR6ljl3TZr0cL|LftEEXqKh7UoJ0yr0vJspWIJGOWTJV0GyqlSxXosFLLMttnYy+upJ0HP+6lnN21N|dTWJBzaV21bDXCs3M6FQRmg=|-----END PRIVATE KEY-----|";}s:7:"client.";a:1:{s:9:"publicKey";s:451:"-----BEGIN PUBLIC KEY-----|MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArb4MFdbQADlDBBHAOu42|vRmzP99m+cWHNWbVh7oQDRMPGKaKafGTgHEmJ3/63FaUETW6mJKIwTFX2b/6+44A|fFrpXSdBM8D1rjldvXfYj45t95yKzHS0gBEvlfNiKaTcJDTy5zf/TaPCdwq8c+YP|BdfOIyySmgGq6Uof6AiFHCTUadZeGAXKuQCPspUYXQPsgFNltMaVyuDlZWQmL8rM|ie0ay4qCt/SDsFcL52SfuE5go2h5YGTeN60IMgON3cTJOaqKz6dJ82DsvVGWlWWS|57B4XW1QyRiIn4C9cT+z10AiLO5bkMVqk42T4BY+ikGjIjxf26sbnA4GBFDXPNZ2|rQIDAQAB|-----END PUBLIC KEY-----|";}}s:9:"security.";a:1:{s:28:"clientHostAddressRestriction";s:0:"";}}',
+            'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
+            'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
+        ],
+    ],
+    'FE' => [
+        'cHashIncludePageId' => true,
+        'debug' => false,
+        'loginSecurityLevel' => 'rsa',
+    ],
+    'GFX' => [
+        'colorspace' => 'sRGB',
+        'im' => 1,
+        'im_mask_temp_ext_gif' => 1,
+        'im_path' => '/usr/bin/',
+        'im_path_lzw' => '/usr/bin/',
+        'im_v5effects' => 1,
+        'im_version_5' => 'im6',
+        'image_processing' => 1,
+        'jpg_quality' => '80',
+    ],
+    'INSTALL' => [
+        'wizardDone' => [
+            'TYPO3\CMS\Install\Updates\AccessRightParametersUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\BackendUserStartModuleUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\Compatibility6ExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\ContentTypesToTextMediaUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\ExtensionManagerTables' => 1,
+            'TYPO3\CMS\Install\Updates\FileListInAccessModuleListUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\FileListIsStartModuleUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\FilesReplacePermissionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\LanguageIsoCodeUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\MediaceExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\MigrateMediaToAssetsForTextMediaCe' => 1,
+            'TYPO3\CMS\Install\Updates\MigrateShortcutUrlsAgainUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\OpenidExtractionUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\PageShortcutParentUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\ProcessedFileChecksumUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\TableFlexFormToTtContentFieldsUpdate' => 1,
+            'TYPO3\CMS\Install\Updates\WorkspacesNotificationSettingsUpdate' => 1,
+            'TYPO3\CMS\Rtehtmlarea\Hook\Install\DeprecatedRteProperties' => 1,
+            'TYPO3\CMS\Rtehtmlarea\Hook\Install\RteAcronymButtonRenamedToAbbreviation' => 1,
+        ],
+    ],
+    'MAIL' => [
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'extbase_object' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
+                    'groups' => [
+                        'system',
+                    ],
+                    'options' => [
+                        'defaultLifetime' => 0,
+                    ],
+                ],
+            ],
+        ],
+        'clearCacheSystem' => false,
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'enableDeprecationLog' => false,
+        'encryptionKey' => 'ab15bc8b61441149c33a59fdaaea1924b53c610f2a3dbaea99e81678be70a26772596fcc17f2f4665fe964dea9f901b1',
+        'exceptionalErrors' => 20480,
+        'isInitialDatabaseImportDone' => true,
+        'isInitialInstallationInProgress' => false,
+        'sitename' => 'caretaker instance',
+        'sqlDebug' => 0,
+        'systemLogLevel' => 2,
+        't3lib_cs_convMethod' => 'mbstring',
+        't3lib_cs_utils' => 'mbstring',
+    ],
+];
